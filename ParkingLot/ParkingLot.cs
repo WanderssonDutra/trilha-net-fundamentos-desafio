efe4c481b31161cba05475e385d16b601bd77d6b
Wandersson Dutra <wanderssongds@gmail.com>
2024-02-01 22:31:08 -0300

    Simple parking lot project

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+static void Main() { }
+namespace TrilhaNetFundamentosDesafio.ParkingLot
+{
+    public class ParkingLot
+    {
+        public List<string> VehiclePlaque = new List<string>();
+        /// <summary>
+        ///  verifica se o usuário digitou uma plaqua válida e adiciona um veículo ao estacionamento.
+        /// </summary>
+        /// <param name="readResult">Plaqua do veículo estacionado.</param>
+        public void AddVehicle(string readResult)
+        {
+            bool validatePlaque = false;
+            if (readResult.Length == 7)
+            {
+                bool validateIndex = true;
+                bool validateNumber = true;
+                //Adiciona os caracteres separados de uma cadeia de caracteres pelo método Substring do valor lido e atribui a uma variável
+                string index = readResult.Substring(0, 4);
+                string numbers = readResult.Substring(4, 3);
+                char[] indexLetters = index.ToCharArray();
+                char[] numbersLetters = numbers.ToCharArray();
+                /* Verifica se cada letra de uma plaqua de veículo digitada pelo usuário está nos conformes requeridos pelo programa.
+                Se sim, adicione o veículo ao estacionamento.*/
+                int validate = 0;
+                for (int count = 0; count < indexLetters.Length; count++)
+                {
+                    if (int.TryParse(Convert.ToString(indexLetters[count]), out validate))
+                        validateIndex = false;
+                }
+                for (int count = 0; count < numbersLetters.Length; count++)
+                {
+
+                    if (!int.TryParse(Convert.ToString(numbersLetters[count]), out validate))
+                        validateNumber = false;
+                }
+                if (validateIndex && validateNumber && index.IndexOf("-") == 3)
+                {
+                    VehiclePlaque.Add(readResult);
+                    Console.WriteLine($"Veículo com a plaqua {readResult} adicionado.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                    validatePlaque = true;
+            }
+            else
+                validatePlaque = true;
+            if (validatePlaque)
+            {
+                Console.WriteLine("Por favor, adicione uma plaqua válida.\n");
+                Console.WriteLine("Formato aceito: ABC-123.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        /// <summary>
+        /// Lista todos os veículos estacionados.
+        /// </summary>
+        /// <param name="vehiclesPlaque">Total de veículos estacionados</param>
+        public void VehiclesParked()
+        {
+            Console.WriteLine("Veículos estacionados: \n");
+            for (int count = 0; count < VehiclePlaque.Count; count++)
+                Console.WriteLine($"Plaqua do veículo: {VehiclePlaque[count]}");
+        }
+
+        /// <summary>
+        /// Remove um veículo estacionado.
+        /// </summary>
+        /// <param name="readResult">Plaqua do veículo.</param>
+        /// <param name="perHourParked">Preço por hora estacionado.</param>
+        /// <param name="hoursParked">Total de horas estacionado</param>
+        /// <param name="initialTax">Taxa inicial ao estacionar.</param>
+        public void VehiclesRemoved(string readResult, int perHourParked, int hoursParked, int initialTax)
+        {
+            /*Verifica se a plaqua de veículo digitada pelo usuário está armazenada na List<string> VehiclePlaque.
+            Se sim, Remova e calcule o total a pagar pelo tempo estacionado*/
+            if (VehiclePlaque.Contains(readResult))
+            {
+                Console.WriteLine($"Veículo com a plaqua {readResult} removido.");
+                VehiclePlaque.Remove(readResult);
+                int payment = perHourParked * hoursParked + initialTax;
+                Console.WriteLine($"Total a pagar: {payment:c}");
+            }
+            else
+                Console.WriteLine("Veículo não encontrado. Verifique se digitou a plaqua corretamente.");
+        }
+    }
+}
+
---
+++ Program/Program.cs
@@ -0,0 +1,89 @@
+﻿/*Programa com a finalidade de adicionar veículos a um estacionamento. Os veículos sáo registrados através de uma List<string> da 
+classe ParkingLot, e seus métodos são utilizados para armazenar, remover, e listar veículos do estacionamento. O programa apresenta
+um menu com opções de 1 a 4 lidos e adicionados a uma variável percorrida em uma instrução Switch. cada case de Switch usará os métodos
+de ParkingLot adequados. O programa também tem a capacidade de validar as repostas do usuário conforme o que for requerido*/
+using System;
+using System.Security.Cryptography.X509Certificates;
+using TrilhaNetFundamentosDesafio.ParkingLot;
+
+ParkingLot vehicle = new();
+bool endTask = true;
+string initialTax;
+string perHourParked;
+int perHourParkedInt = 0;
+int initialTaxInt = 0;
+do
+{
+    Console.WriteLine("Digite a taxa inicial: \n");
+    initialTax = Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("\nDigite o preço por hora estacionda: \n");
+    perHourParked = Console.ReadLine();
+    Console.Clear();
+    //Verifica se os valores digitados são válidos.
+    if (int.TryParse(initialTax, out initialTaxInt) && int.TryParse(perHourParked, out perHourParkedInt))
+        endTask = false;
+    else
+        Console.WriteLine("Comando inválido. Por favor, adicione um número válido");
+    Console.ReadKey();
+    Console.Clear();
+} while (endTask);
+endTask = true;
+do
+{
+    Console.WriteLine(@"=========================
+Estacionmento de veículos
+=========================
+");
+    Console.WriteLine(@"MENU DE OPÇÕES
+
+1. Cadastrar Veículo.
+2. Remover Veículo.
+3. Listar Veículo.
+4. Encerrar.");
+    string readResult = Console.ReadLine();
+    Console.Clear();
+
+    switch (readResult)
+    {
+        //Cadastra um veículo através da placa.
+        case "1":
+            Console.WriteLine("Digite a placa do veículo: \n");
+            readResult = Console.ReadLine().ToUpper().Trim();
+            Console.Clear();
+            vehicle.AddVehicle(readResult);
+            break;
+        /*Remove um veículo do estacionamento e cálcula o total a pagar baseado na quantidade de 
+        horas estacionado x preço por hora + taxa inicial.*/
+        case "2":
+            Console.WriteLine("Digite a plaqua do veículo que deseja remover: \n");
+            string readResult2 = Console.ReadLine().ToUpper().Trim();
+            Console.WriteLine("Quantidade de horas estacionado: \n");
+            string hoursParked = Console.ReadLine();
+            Console.Clear();
+            int hoursParkedInt = 0;
+            //Verifica se o valor digitado é válido.
+            if (int.TryParse(hoursParked, out hoursParkedInt))
+                vehicle.VehiclesRemoved(readResult2, perHourParkedInt, hoursParkedInt, initialTaxInt);
+            else
+                Console.WriteLine("Comando inválido. Por favor, adicione um número válido.");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        //Lista os veículos estacionados.
+        case "3":
+            vehicle.VehiclesParked();
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        case "4":
+            Console.WriteLine("Programa encerrado.");
+            endTask = false;
+            break;
+        default:
+            Console.WriteLine("Comando inválido.");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+    }
+} while (endTask);
